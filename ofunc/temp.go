8f521fcabb683c53a031f3744efce3448b27dc5a
guojia <guojia@guojia.com>
2023-08-11 11:26:43 +0800

    f

--- ofunc/temp.go
+++ ofunc/temp.go
@@ -6,8 +6,27 @@
 
 package ofunc
 
+import (
+	"errors"
+	"reflect"
+)
+
 type TempsFunc interface {
 	Call(args ...interface{}) (out []interface{})
 	Update(fn interface{}) error
 	Delete(fn interface{})
+}
+
+type tempFunc struct {
+	callMap map[string]reflect.Value
+}
+
+func (t *tempFunc) checkFn(fn interface{}) (string, reflect.Value, error) {
+	val := reflect.ValueOf(fn)
+	typ := reflect.TypeOf(fn)
+
+	if typ.Kind() != reflect.Func {
+		return "", reflect.Value{}, errors.New("input fn value is not func")
+	}
+	typ.
 }
