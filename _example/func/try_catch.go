522705d3830d09bdc893c5d586f92b724839e19c
guozejia <guozejia@bingosoft.net>
2023-11-02 13:56:23 +0800

    修改try实现

--- _example/func/try_catch.go
+++ _example/func/try_catch.go
@@ -8,23 +8,51 @@ import (
 )
 
 func main() {
+
+	//// The normal processing process is to only process one
+	//ofunc.Try(func() {
+	//	// todo code
+	//	// Empty errors will not be handled, you can easily place your errors directly in Throw
+	//	var err error
+	//	ofunc.Throw(err)
+	//}).Catch(func(err ofunc.StackTraceErr) {
+	//	fmt.Println(err)
+	//})
+	//
+	//// Multiple function chain execution will not interfere with each other Try
+	//// When you have many Try, please use CatchAll to catch errors
+	//ofunc.Try(func() {
+	//	// todo code
+	//	// Empty errors will not be handled, you can easily place your errors directly in Throw
+	//	var err error
+	//	ofunc.Throw(err)
+	//}).Try(func() {
+	//	// todo code
+	//	panic(errors.New("test error by panic"))
+	//}).Try(func() {
+	//	// todo code
+	//	// The Throw function will determine whether error is empty. If it is empty,
+	//	// it will interrupt the subsequent execution of the function.
+	//	ofunc.Throw("test error by throw")
+	//	fmt.Println("this code will not be executed ")
+	//}).Try(func() {
+	//	// todo code
+	//	a := 0
+	//	b := 0
+	//	fmt.Println(a / b)
+	//}).CatchAll(func(errs []ofunc.StackTraceErr) {
+	//	for i := 0; i < len(errs); i++ {
+	//		fmt.Println(errs[i])
+	//	}
+	//})
+
+	// If you only need to catch certain errors, you can use interceptors
+	var DataError = errors.New("test data error")
 	ofunc.Try(func() {
-		// todo code
-		var err = errors.New("test error by panic")
-		if err != nil {
-			panic(err)
-		}
+		ofunc.Throw(DataError)
 	}).Try(func() {
-		ofunc.Throw("test error by throw")
-	}).Catch(func(err error) {
-		// todo code by error
+		ofunc.Throw("todo error")
+	}).CatchWithErr(DataError, func(err ofunc.StackTraceErr) {
 		fmt.Println(err)
-	}).CatchTrace(func(err error, stack ofunc.StackTraces) {
-		// todo code by detailed error
-		fmt.Println(stack.String())
-	}).Finally(func() {
-		fmt.Println("finally")
-	}).Finally(func() {
-		fmt.Println("finally2")
 	})
 }
---
+++ ofunc/try_catch_trace.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+type (
+	StackTrace struct {
+		pc   uintptr
+		file string
+		line int
+	}
+	StackTraceErr struct {
+		StackTraces []StackTrace
+		Err         error
+	}
+)
+
+func (s StackTraceErr) String() string {
+	out := fmt.Sprintf("%+v error stack traces\n", s.Err)
+	for _, ss := range s.StackTraces {
+		f := runtime.FuncForPC(ss.pc)
+		out += fmt.Sprintf("%s\n\t%s:%d\n", f.Name(), ss.file, ss.line)
+	}
+	return out
+}
